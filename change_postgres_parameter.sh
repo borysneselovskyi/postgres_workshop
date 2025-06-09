0c801a1046cf88d800162182543f0705e49b858c
borysneselovskyi <70379260+borysneselovskyi@users.noreply.github.com>
2025-06-09 11:18:29 +0200

    Update change_postgres_parameter.sh

--- change_postgres_parameter.sh
+++ change_postgres_parameter.sh
@@ -5,12 +5,12 @@ cp ${PGDATA}/postgresql.conf ${PGDATA}/postgresql.conf_back
                                                                                                                                  
 echo -e "\nParameter shared_preload_libraries - old value:"                                                                      
 grep shared_preload ${PGDATA}/postgresql.conf                                                                                    
-                                                                                                                                 
+                                                                                                                                                                                                                                                                  
+sed -i 's/#shared_preload_libraries.*/shared_preload_libraries='\''pg_stat_statements'\''/' ${PGDATA}/postgresql.conf
+
 echo -e "\nParameter shared_preload_libraries - new value:"                                                                      
-grep shared_preload ${PGDATA}/postgresql.conf                                                                                    
-                                                                                                                                 
-sed -i 's/#shared_preload_libraries.*/shared_preload_libraries='\''pg_stat_statements'\''/' ${PGDATA}/postgresql.conf           
-                                                                                                                                 
+grep shared_preload ${PGDATA}/postgresql.conf 
+
 echo -e "\nRestart the postgres server:"                                                                                         
 echo "/usr/lib/postgresql/17/bin/pg_ctl restart -D ${PGDATA}"                                                                    
 /usr/lib/postgresql/17/bin/pg_ctl restart -D ${PGDATA}   
